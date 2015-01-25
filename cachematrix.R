dcde50357ecbd9c37dec18f18fc84d2b12d8a000
llylite <llylite@gmail.com>
2015-01-24 21:56:22 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,11 @@
-## These functions can solve the inverse of a matrix and cache the
-## inverse matrix together with original, to save computation time
+## These functions together solve the inverse of a matrix and cache the inverse matrix together 
+## with the original one, to save the long computation time for the solve inverse function
 
-## This function converts a matrix into a dataframe, which can 
-## be used to cache or solve the inverse of the original matrix
+## makeCacheMatrix creates a special "matrix", which is really a list containing a function to
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse
+## get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -18,9 +21,11 @@ makeCacheMatrix <- function(x = matrix()) {
                  getsolve = getsolve)
 }
 
+## cacheSolve calculates the inverse of the special "matrix" created with the above function. 
+## However, it first checks to see if the inverse has already been calculated. If so, it gets 
+## the inverse from the cache and skips the computation. Otherwise, it calculates the inverse
+## of the data and sets the value of the inverse in the cache via the setsolve function.
 
-## This function takes a dataframe from the above function, and 
-## return the inverse of matrix in x
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
